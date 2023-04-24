447cfe0b596cb33a0a3ff217aafcdb365a6e1e35
JULIÁN WEGMAN <47806018@comunidadort.edu.ar>
2023-04-24 08:09:07 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+
+        int eleccion=Menu();
+        while(eleccion<5){
+            switch(eleccion){
+                case 1:
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 4:
+                break;
+            }
+        }  
+    }
+    static void Inscripcion(){
+
+
+    }
+    static int Menu(){
+        Console.WriteLine("1- Nueva Inscripción");
+        Console.WriteLine("2- Obtener Estadísticas del Evento");
+        Console.WriteLine("3- Buscar Cliente");
+        Console.WriteLine("4- Cambiar entrada de un Cliente");
+        Console.WriteLine("5- Salir");
+        int eleccion=IngresarInt("");
+        return eleccion;
+    }
+    static int IngresarInt(string mensaje){
+        Console.WriteLine(mensaje);
+        int num=int.Parse(Console.ReadLine());
+        return num;
+    }
+}
